e29513024a3119be45924edf265752a2b92154f2
Thomas Boulund Meng Hansen <tbmh@easv.dk>
2024-01-26 10:49:00 +0100

    Prepared example

---
+++ ClassDemo/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine(GetGreetingMessage());
+    }
+
+    public static string GetGreetingMessage()
+    {
+        bool isDroneCI = Environment.GetEnvironmentVariable("DRONE") == "true";
+
+        if (isDroneCI)
+        {
+            return "Hello, Drone CI!";
+        }
+        else
+        {
+            return "Hello, World!";
+        }
+    }
+}
---
+++ UnitTests/ProgramTest.cs
@@ -0,0 +1,20 @@
+
+using Xunit;
+
+namespace UnitTests;
+
+public class ProgramTest
+{
+    [Fact]
+    public void GetGreetingMessage_ShouldReturnDroneCI_GivenDroneCIBranch()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("DRONE", "true");
+
+        // Act
+        var result = Program.GetGreetingMessage();
+
+        // Assert
+        Assert.Equal("Hello, Drone CI!", result);
+    }
+}
